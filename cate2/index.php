2648ad0de4c1db635dfed5c1a0799ce17c224dc4
idben <idben.chen@gmail.com>
2024-06-06 12:37:07 +0800

    修正額外載入的 css 和 js 路徑

--- cate2/index.php
+++ cate2/index.php
@@ -1,8 +1,8 @@
 <?php
   include "../vars.php";
   $cateNum = 1;
-  $cssList = ["./cate2/cate2.css"];
-  $jsList = ["./cate2/cate2.js"];
+  $cssList = ["../cate2/cate2.css"];
+  $jsList = ["../cate2/cate2.js"];
   $pageTitle = "{$cate_ary[$cateNum]} 管理";
   include "../template_top.php";
   include "../template_nav.php";
